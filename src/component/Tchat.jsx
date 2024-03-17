d6d0be845a81b642e821080fbdd4228bdb1b60d6
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-03-17 14:22:11 -0700

    users

--- src/component/Tchat.jsx
+++ src/component/Tchat.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 function Tchat() {
-
 const logout=()=>{
  localStorage.clear()
  window.location.reload()
 }
-
   return (
     <div>
       <form action="">
+        <h1>name of a user</h1>
         <input type="text" placeholder='write a message'/>
         <button>send</button>
       </form>
