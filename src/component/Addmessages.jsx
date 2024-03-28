530d2b34c52c53a2c86049119cd425eb1c8f815d
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-03-28 04:57:56 -0700

    adds

--- src/component/Addmessages.jsx
+++ src/component/Addmessages.jsx
@@ -5,11 +5,18 @@ function Addmessages() {
 // pushing data
 const handlesend=(e)=>{
     setMessagesent(e.target.value)
+    
 }
+const handlesubmit=(e)=>{
+   e.preventDefault()
+   if(messageSent !==""){
+      alert("its oky")
+   }
+  }
   return (
     <div>
       <h1>add messages</h1>
-      <form>
+      <form onSubmit={handlesubmit}>
       <input type="text" id={messageSent} value={messageSent} onChange={handlesend}/>
       <button type='submit'>send message</button>
       </form>
